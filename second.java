e43d6413e4dfc2416a6ce99a0073b4e8128010d3
Chetna <rathorechetna03@gmail.com>
2023-03-29 17:30:02 +0530

    add sum functionaltiy

--- second.java
+++ second.java
@@ -3,5 +3,17 @@
 public class second {
     public static void main (String args[]){
         System.out.println("Welcome For Project File");
+
       } 
+
+
+}
+
+class Third extends second{
+    void add(){
+        int a=15;
+        int b=20;
+        int c=0;
+        System.out.println("Sum :"+c);
+    }
 }
